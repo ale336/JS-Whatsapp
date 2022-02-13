3a06cf1b13d4f79213c5508ba3b388b2e561a814
Alejandro Ramos <caramos336@gmail.com>
2022-02-13 08:25:36 -0300

    Commit Inicial

---
+++ lab2.js
@@ -0,0 +1,5 @@
+'use strict'
+var nombre = prompt("ingresa nombre:");
+var ape = prompt("ingresa apellido:");
+var edad = prompt("ingresa edad:");
+alert("El nombre del usuario es: "+ nombre + " " + ape);
