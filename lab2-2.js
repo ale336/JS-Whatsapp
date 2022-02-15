2a06a0c85fd9f4a117626241a2369104a38f02e5
ale336 <71363681+ale336@users.noreply.github.com>
2022-02-15 01:12:39 -0300

    NUEVOS EJERCICIOS LAB2

---
+++ lab2-2.js
@@ -0,0 +1,7 @@
+'use strict'
+var nombre = prompt("ingresa nombre:");
+var ape = prompt("ingresa apellido:");
+var edad = prompt("ingresa edad:");
+document.write("El nombre del usuario es: "+ nombre + " " + ape);
+document.write("<br>");
+document.write("La edad del usuario es:"+ edad);
